d29daa9e54dcedbc99136c62e8e7385896a2971d
snowlotus <252805468@qq.com>
2014-08-03 22:20:39 +0800

    add function for Date

--- src/smart.date.js
+++ src/smart.date.js
@@ -1,20 +1,37 @@
-var $D={
-	DP:Date.prototype,
-	isDate:function(d){
+var $D = {
+	DP: Date.prototype,
+	isDate: function(d) {
 		return d instanceof Date;
-	}
+	},
 	/**
 	 * 求两日期相隔天数
-	 * @param  {Date} d1 
-	 * @param  {Date} d2 
+	 * @param  {Date} d1
+	 * @param  {Date} d2
 	 * @return {Number}    两日期相隔的天数
 	 */
-	getDatePeriod:function(d1,d2){
-		if(this.isDate(d1)||(this.isDate(d2))){
+	getDatePeriod: function(d1, d2) {
+		if (!this.isDate(d1) || !(this.isDate(d2))) {
 			throw new TypeError('it is not date type');
 		}
-		var timeDiff=Math.abs(d1.getTime()-d2.getTime());
-		var diffDays=Math.ceil(timeDiff/(1000*3600*24));
+		var timeDiff = Math.abs(d1.getTime() - d2.getTime());
+		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 		return diffDays;
 	},
+
+	/**
+	 * 是否是闰年
+	 * @param  {Date}  d
+	 * @return {Boolean}   true:闰年;false:否
+	 */
+	isLeapYear: function(d) {
+		if (!this.isDate(d)) {
+			throw new TypeError('it is not date type');
+		}
+		var year = d.getFullYear();
+		return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+	},
+
+	dateFormat:function(d,formatStr){
+
+	}
 }
