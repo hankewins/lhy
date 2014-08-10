6317970ab1e09cd707bac7686a03216784cf20c9
snowlotus <252805468@qq.com>
2014-08-10 21:57:02 +0800

    add cookie module

--- src/smart.browser.js
+++ src/smart.browser.js
@@ -3,12 +3,6 @@ smart.package(function(smart) {
 	var doc = this.document,
 		win = doc.window = this;
 
-	//Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0
-	//Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)
-	//Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36
-	//Opera/9.80 (Windows NT 5.1; U; en) Presto/2.8.119 Version/11.10
-	//Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-HK) AppleWebKit/533.18.1 (KHTML, like Gecko) Version/5.0.2 Safari/533.18.5
-
 	/**
 	 * 从UA和平台信息中提取出当前的浏览器名称、版本、引擎、系统等信息
 	 * 参考链接 https://developer.mozilla.org/en-US/docs/Browser_detection_using_the_user_agent
@@ -19,16 +13,13 @@ smart.package(function(smart) {
 	 */
 	var parseUA = function(ua, platform) {
 		var name, version, engine;
+		/Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0
 		ua = ua.toLowerCase();
 		platform = platform ? platform.toLowerCase() : '';
 		//attention:Safari gives two version number, one technical in the Safari/xyz token, one user-friendly in a Version/xyz token
 		var reg = /(opera|ie|firefox|chrome|trident|version)[\s\/:]([\w\d\.]+)?.*?(safari|(?:rv[\s\/:]|version[\s\/:])([\w\d\.]+)|$)/;
 		var UA = ua.match(reg) || [null, 'unkonwn', 0];
-		//["chrome/36.0.1985.125 safari", "chrome", "36.0.1985.125", "safari", undefined]
 		//["firefox/31.0", "firefox", "31.0", "", undefined]
-		//["opera/9.80 (windows nt 5.1; u; en) presto/2.8.119 version/11.10", "opera", "9.80", "version/11.10", "11.10"]
-		//["ie 8.0; windows nt 6.1; trident/4.0; slcc2; .net clr 2.0.50727; .net clr 3.5.30729; .net clr 3.0.30729; media center pc 6.0; .net4.0c; .net4.0e)", "ie", "8.0", "", undefined]
-		//["version/5.0.2 safari", "version", "5.0.2", "safari", undefined]
 
 
 		if (UA[1] == 'trident') {
@@ -119,7 +110,6 @@ smart.package(function(smart) {
 		width:win.screen.width,
 		height:win.screen.height,
 		dpi:win.devicePixelRatio
-
 	}
 
 	smart.browser = browser;
--- src/smart.cookie.js
+++ src/smart.cookie.js
@@ -1,1 +1,45 @@
-// Todo
+/** 
+ * @description
+ * Package: smart.cookie
+ * Need package:smart.core.js
+ */
+;
+smart.package(function(smart) {
+	var doc = document,
+		win = window = this;
+	var cookie = {
+		options: {
+			path: '/',
+			encode: true,
+			secure: false,
+			domain: false,
+			expires: false
+		},
+		setItem: function(sKey, value /*options*/ ) {
+			var opt = smart.extend(arguments[2], this.options);
+			if (opt.encode) value = encodeURIComponent(value);
+			if (opt.path) value += '; path=' + opt.path;
+			if (opt.domain) value += '; domain=' + opt.domain;
+			if (opt.expires) {
+				var date = new Date();
+				date.setTime(date.getTime() + opt.expires * 24 * 60 * 60 * 1000);
+				value += '; expires=' + date.toGMTString();
+			}
+			if (opt.secure) value += '; secure';
+			doc.cookie = sKey + '=' + value;
+
+		},
+		getItem: function(sKey /* options*/ ) {
+
+		},
+		removeItem: function(sKey /*options*/ ) {
+
+		},
+		hasItem: function(sKey) {
+
+		}
+
+
+	};
+	smart.cookie = cookie;
+});
