f6f494264d81b71fea3cc520ab3dbadf0a43847a
snowlotus <252805468@qq.com>
2014-08-10 22:32:07 +0800

    update by hxl -modify cookie module

--- src/smart.cookie.js
+++ src/smart.cookie.js
@@ -10,14 +10,17 @@ smart.package(function(smart) {
 	var cookie = {
 		options: {
 			path: '/',
-			encode: true,
 			secure: false,
 			domain: false,
 			expires: false
 		},
+		_setOptions:function(opt){
+			if(opt==undefined) return this.options;
+			return smart.extend(opt,this.options);
+		},
 		setItem: function(sKey, value /*options*/ ) {
-			var opt = smart.extend(arguments[2], this.options);
-			if (opt.encode) value = encodeURIComponent(value);
+			var opt=this._setOptions(arguments[2]);
+			value = encodeURIComponent(value);
 			if (opt.path) value += '; path=' + opt.path;
 			if (opt.domain) value += '; domain=' + opt.domain;
 			if (opt.expires) {
@@ -29,17 +32,27 @@ smart.package(function(smart) {
 			doc.cookie = sKey + '=' + value;
 
 		},
-		getItem: function(sKey /* options*/ ) {
+
+		getItem: function(sKey) {
+			var value = doc.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(skey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1");
+			return value ? decodeURIComponent(value) : null;
 
 		},
+		
 		removeItem: function(sKey /*options*/ ) {
+			if (!sKey || !this.hasItem(sKey)) return false;
+			var opt=this._setOptions(arguments[1]),
+				value = '';
+			value = "; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+			if (opt.domain) value += "; domain=" + opt.domain;
+			if (opt.path) value += "; path=" + opt.path;
+			doc.cookie = encodeURIComponent(sKey) + "=" + value;
+			return true;
 
 		},
 		hasItem: function(sKey) {
-
+			return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\+")).test(doc.cookie);
 		}
-
-
 	};
 	smart.cookie = cookie;
 });
