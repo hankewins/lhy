cd5dbd9a0b16453dd696e327d631e37f4004b1d3
hankewins <hankewins@gmail.com>
2014-08-11 18:57:47 +0800

    update

---
+++ src/smart.http.js
@@ -0,0 +1,11 @@
+// @import smart.core.js
+// smart.http contains http params ajax
+smart.package(function(smart){
+    var http = {
+	    ajax: function(option){
+	    	
+	    }    	
+    };
+
+    smart.http = http;
+});
